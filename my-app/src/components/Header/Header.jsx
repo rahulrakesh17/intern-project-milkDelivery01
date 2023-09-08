4ada798a54bf1d0bd05102c277915e03ddc5d87d
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-09-08 12:30:49 +0530

    usual comit to save

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
 import "./Header.css"
+import { FaLocationPin } from "react-icons/fa6"
+
 function Header() {
-  return (
-    <>
-       <div className='top-bar section'>
-        <div className='row'>
-            <div className='col-11'>
-                <div className="row">
-                    <div className='col-3'></div>
-                    <div className='col-6'></div>
-                    <div className='col-3'></div>
+    return (
+        <>
+            <div className='top-bar'>
+                <div className="row top-bar-container vertical-center" style={{width:"100%"}}>
+                    <div className='col-xl-3 '>
+                        <div>
+                            <a href="#" >
+                                <div style={{ display: "flex", flexWrap: "nowrap", gap: "5px",paddingTop:"10px"}}>
+                                    <div><FaLocationPin /></div>
+                                    <p>Locate Stores</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='col-xl-6 '>
+                        <a href="#">
+                            <span>FLASH SALE THIS WEEK - OFF 5% ITEMS</span>
+                            <a>Check it now</a>
+                        </a>
+                        
+                    </div>
+                    <div className='col-xl-3 '>g9g8oirthoihoitehroiu</div>
                 </div>
             </div>
-        </div>
-       </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Header
