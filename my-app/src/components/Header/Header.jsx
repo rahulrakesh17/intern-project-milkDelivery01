e5c866255d38d574d3aff656ee9a942cada9aee1
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-09-08 11:24:00 +0530

    usual commit

---
+++ my-app/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Header.css"
+function Header() {
+  return (
+    <>
+       <div className='top-bar section'>
+        <div className='row'>
+            <div className='col-11'>
+                <div className="row">
+                    <div className='col-3'></div>
+                    <div className='col-6'></div>
+                    <div className='col-3'></div>
+                </div>
+            </div>
+        </div>
+       </div>
+    </>
+  )
+}
+
+export default Header
