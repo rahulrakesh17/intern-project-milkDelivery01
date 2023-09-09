06ddbcf39481fdc9c34be560f09f195e21ca2967
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-09-09 13:00:28 +0530

    gjc

--- my-app/src/components/Carousel/Carousel.jsx
+++ my-app/src/components/Carousel/Carousel.jsx
@@ -7,13 +7,13 @@ function Carousel() {
             <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active" data-bs-interval="10000">
-                <img src="..." class="d-block w-100" alt="..."/>
+                <img src="https://media.istockphoto.com/id/1297005860/photo/raw-milk-being-poured-into-container.jpg?s=612x612&w=0&k=20&c=5Xumh49_zYs9GjLkGpZXM41tS17K8M-svN9jLMv0JpE=" class="carousel-img d-block w-100" alt="..."/>
                 </div>
                 <div class="carousel-item" data-bs-interval="2000">
-                <img src="..." class="d-block w-100" alt="..."/>
+                <img src="https://media.istockphoto.com/id/1171812310/photo/young-woman-drinking-milk.jpg?s=612x612&w=0&k=20&c=53AP4la-uLmdEmuAF1Rm-vHmCYWtDRIaXAgIbaHXC4c="  class="carousel-img d-block w-100" alt="..."/>
                 </div>
                 <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="..."/>
+                <img src="https://media.istockphoto.com/id/1297005217/photo/farmer-pouring-raw-milk-into-container.jpg?s=612x612&w=0&k=20&c=toiruwu04HHkwnLZhpKEjNPqOHI7Kmut5NMGTFSGm4M="  class="carousel-img d-block w-100" alt="..."/>
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
