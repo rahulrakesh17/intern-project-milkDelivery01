dba19ae271ba4401f9a1469100efac97c64f84a1
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-09-09 12:45:15 +0530

    vcvcv

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Contextprovider } from './context/Context';
+import {Home} from "./pages/Home"
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Contextprovider>
           <BrowserRouter>
             <Routes>
-              <Route path='/' element={<Header/>}/>
+              <Route path='/' element={<Home/>}/>
             </Routes>
           </BrowserRouter>
 
---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Carousel from '../components/Carousel/Carousel'
+function Home() {
+  return (
+    <>
+        <Header/>
+        <Carousel/>
+    </>
+  )
+}
+
+export default Home
