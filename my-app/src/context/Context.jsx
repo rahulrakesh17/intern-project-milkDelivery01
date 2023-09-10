d08bf5dcabc475a0fd652b666d012555291fa19a
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-09-10 20:04:02 +0530

    sdds

--- my-app/src/context/Context.jsx
+++ my-app/src/context/Context.jsx
@@ -1,22 +1,36 @@
 import React from 'react'
+import firebaseConfig from '../firebase/firebase'; 
+import {initializeApp} from "firebase/app";
+import {collection ,getDocs, doc, addDoc,deleteDoc, getFirestore} from "firebase/firestore"
 import {useState ,useEffect ,createContext,useRef} from "react"
 import data from "../data/CarouselData.json"
 const contextApi = createContext();
 
 const Contextprovider=({children})=>{
   const [carouselData,setCarouselData]=useState(data);
-
-
   const [SideNav,setSideNav]=useState(false)
   const handleSideNav=()=>{
     setSideNav(!SideNav)
   }
+   
+  initializeApp(firebaseConfig)
+  const db = getFirestore();
+  const colRef = collection(db,"products");
+  const [products,setProducts]=useState([])
 
+  getDocs(colRef)
+  .then((snapshot)=>{
+      snapshot.docs.forEach(item=>{
+          products.push({...item.data(),id:item.id})
+      })
+  })
+  console.log(products)
   return(
     <contextApi.Provider value={{
       handleSideNav,
       SideNav,
       carouselData,
+      products
 
     }}>{children}</contextApi.Provider>
   )
---
+++ my-app/src/firebase/firebase.jsx
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCW9-fERekxCazs87-R25nHWtJJRn68hY",
+    authDomain: "milkdelivery-project.firebaseapp.com",
+    projectId: "milkdelivery-project",
+    storageBucket: "milkdelivery-project.appspot.com",
+    messagingSenderId: "811271788810",
+    appId: "1:811271788810:web:fa7db73fd805b0675e82f3",
+    measurementId: "G-JENQQP73MD"
+  };
+export default firebaseConfig
