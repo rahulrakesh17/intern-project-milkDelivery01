676baed7348efc4232dac2eec10eccec1507d59f
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-09-10 08:39:17 +0530

    dccd

--- my-app/src/context/Context.jsx
+++ my-app/src/context/Context.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {useState ,useEffect ,createContext,useRef} from "react"
-
-
+import data from "../data/CarouselData.json"
 const contextApi = createContext();
 
 const Contextprovider=({children})=>{
+  const [carouselData,setCarouselData]=useState(data);
+
+
   const [SideNav,setSideNav]=useState(false)
   const handleSideNav=()=>{
     setSideNav(!SideNav)
@@ -13,7 +15,8 @@ const Contextprovider=({children})=>{
   return(
     <contextApi.Provider value={{
       handleSideNav,
-      SideNav
+      SideNav,
+      carouselData,
 
     }}>{children}</contextApi.Provider>
   )
