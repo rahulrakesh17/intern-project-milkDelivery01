ee5d1c2cefedd3b5abe801f966598865652c4ec7
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-09-10 20:16:56 +0530

    vv

--- my-app/src/components/mustTry/Musttry.jsx
+++ my-app/src/components/mustTry/Musttry.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./Musttry.css"
+import { useContext,useState } from 'react'
+import { contextApi } from '../../context/Context'
+
+
 function Musttry() {
+  const useContextApi = useContext(contextApi)
+  console.log(useContextApi.products)
   return (
-    <div>
-      
+    <div className='container'>
+      <div className='row'>
+        di <h2>MUST TRY</h2>
+        <div className='must-line'></div>
+      </div>
+      <div className="row">
+          <div className="col-lg-4">
+            <img className="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140"/>
+            <h2>Heading</h2>
+            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+            <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+        </div>
     </div>
   )
 }
