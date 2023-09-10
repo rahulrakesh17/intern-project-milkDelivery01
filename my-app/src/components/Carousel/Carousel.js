2400d41573ab151baea3bdac561e7b0a40ea0421
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-09-10 08:46:00 +0530

    cdcd

--- my-app/src/components/Carousel/Carousel.js
+++ my-app/src/components/Carousel/Carousel.js
@@ -1,10 +1,17 @@
 import "./Carousel.css"
 import React from 'react'
+import { useContext } from "react"
+import { contextApi } from "../../context/Context"
 
 function Carousel() {
+        const useContextApi = useContext(contextApi);
+        console.log(useContextApi.carouselData.slides)
         return(
+            
             <div>
-                Carousel
+                {useContextApi.carouselData.slides.map((item,index)=>{
+                    return <img src={item.src} alt={item.alt} key={index}></img>
+                })}
             </div>
 
         )
