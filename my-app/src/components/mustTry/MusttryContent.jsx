84f208a1343203e6be7318061ee1c98c0a19458e
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-09-11 19:48:44 +0530

    f

--- my-app/src/components/mustTry/MusttryContent.jsx
+++ my-app/src/components/mustTry/MusttryContent.jsx
@@ -20,7 +20,7 @@ function MusttryContent() {
               <div className='my-card title'><h3 style={{textAlign:"center",fontSize:"small"}}>{item.title}</h3></div>
               <div className='my-card price'><p style={{textAlign:"center"}}>₹{item.price}</p></div>
               
-              <div className='row' key={index}>
+              {/* <div className='row' key={index}>
                 <div className='col-12'>
                   <div className='row'>
                     <div className='col-10'>
@@ -28,7 +28,7 @@ function MusttryContent() {
                     </div>
                   </div>
                 </div>
-              </div>
+              </div> */}
 
               </div>
           
